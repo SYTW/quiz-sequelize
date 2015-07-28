c473317ec3586d1aa7385d115fbf00b4af0b1800
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2015-07-28 11:18:58 +0100

    autoload completado m7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,24 +1,35 @@
 var models = require('../models/models.js');
 var Quiz = models.Quiz;
 
+// Autoload: factoriza el código si la ruta incluye quizId
+exports.load = function(req, res, next, quizId) {
+  Quiz.find(quizId).then( 
+      function(quiz) {
+        if (quiz) {
+          req.quiz = quiz;
+          next();
+        } else { next(new Error('No existe quizId = '+quizId)); }
+      }
+      ).catch( function(error) { next(error); });
+};
+
+// GET /quizes/:id
 exports.show = function(req, res) {
-  Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizes/show', { quiz: quiz });
-  });
+  res.render('quizes/show', { quiz: req.quiz });
 };
 
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-  Quiz.find(req.params.quizId).then(function(quiz) {
-    var respuesta = 'Incorrecto';
-    if (req.query.respuesta === quiz.respuesta) respuesta = 'Correcto'; 
-    res.render('quizes/answer', { quiz: quiz, respuesta: respuesta })
-  });
+  var respuesta = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) respuesta = 'Correcto'; 
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: respuesta })
 };
 
+// GET /quizes
 exports.index = function(req, res) {
   Quiz.findAll().then(function(quizes){
     res.render('quizes/index.ejs', { quizes: quizes});
-  });
+  }).catch(function(error) { next(error); });
 };
 
 exports.author = function(req, res) {
